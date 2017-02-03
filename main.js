8b2852fcb64a3409943b9099a9df6c28245d95ac
tonysauvageau <toneeees@gmail.com>
2017-02-02 20:37:05 -0700

    updated pages, added contact

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var fs = require('fs');
 
 var app = express();
 
+
+var contactFileName = './contact_info.json';
+var contactFile = require(contactFileName);
+
 var port = 8000;
 
 //get, put, post, delete
@@ -28,10 +32,39 @@ app.get('/courses/:id', function ( req, res ) {
         var course = courseParse.filter(function(p){
             return p.id === parseInt(req.params.id);
         })[0];
-        res.locals = { course: course }
-        res.render('course.ejs');
+
+        fs.readFile('reviews.json', 'utf8', function ( err, data) {
+            var reviewParse = JSON.parse(data);
+            var review = reviewParse.filter(function(r){
+            return r.course_id === parseInt(req.params.id);
+            })[0];
+            res.locals = { course: course, review: review }
+            res.render('course.ejs');
+
+        });     
     });
+    
+
 });
+
+app.get('/contact', function(req, res){
+   res.render('contact.ejs');
+});
+
+
+app.post('/contact/add', function(req, res){
+    var contact_obj = {
+        "name" : req.params.name,
+        "phone" : req.params.number
+    };
+
+    fs.writeFile(contactFileName, JSON.stringify(contact_obj), function (err) {
+        if(err){ console.log(err); }
+       console.log();
+    });
+    
+});
+
 
 app.listen(port);
 console.log('Server listening on http://localhost:'+port+' !');
